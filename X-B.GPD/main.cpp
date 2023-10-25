31fdeaeb9831293f512c683a672d7678eac4d3ab
galpat123 <148947771+galpat123@users.noreply.github.com>
2023-10-25 10:33:38 +0300

    Add files via upload

---
+++ X-B.GPD/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int szam;
+    cout<<"szam="
+    cin>>szam;
+    if(szam%10==0) {
+            cout<<"Paros";
+    }else{
+        cout
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
